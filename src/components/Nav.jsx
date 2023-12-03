fbba5c16c862d3406f97e25ae0e2fcebd2006f8e
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2023-12-03 08:08:51 -0300

    arreglo de componente nav y estilos

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import SearchBar from "./SearchBar"
 import styles from "./styles.module.css"
 
-export default function Nav(){
+export default function Nav({funcion}){
     return <div className={styles.nav}>
         <div className={styles.header}>
             <h1>OpenWeather</h1>
         </div>
-        <SearchBar />
+        <SearchBar search={funcion}/>
     </div>
 }
